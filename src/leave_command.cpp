1e43cd10e6d8f321b7e065c1aad2915c0df73001
progress <nikita15933@mail.ru>
2025-11-29 12:00:35 +0300

    update leave_command

--- src/leave_command.cpp
+++ src/leave_command.cpp
@@ -2,4 +2,29 @@
 
 void LeaveCommand::execute(const dpp::slashcommand_t &event)
 {
+   event.thinking();
+
+   auto &musicHandler = bot.getMusicHandler(event.command.guild_id);
+   dpp::voiceconn *v = event.from()->get_voice(event.command.guild_id);
+
+   if (!v)
+   {
+      event.edit_response("You are not in the voice channel!");
+      return;
+   }
+   
+   if(!v->is_active())
+   {
+      event.edit_response("The bot has not finished connecting!");
+      return;
+   }
+
+   if(musicHandler.voiceclient && musicHandler.voiceclient->is_playing())
+   {
+      musicHandler.voiceclient->stop_audio();
+      musicHandler.setStopFlag(true);
+   }
+
+   event.from()->disconnect_voice(v->guild_id);
+   event.edit_response("The bot has left the channel!");
 }
